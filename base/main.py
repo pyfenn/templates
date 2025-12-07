fa01b6c8fcb2b34ac48e84ce24b010885d631abd
blkdmr <alessio.russo.ar98@gmail.com>
2025-12-07 12:23:23 +0100

    updated base tempale

--- base/main.py
+++ base/main.py
@@ -1,19 +1,11 @@
 from fenn import FENN
-from fenn.notification import Notifier
-from fenn.notification.services import Discord, Telegram
 
 app = FENN()
-notifier = Notifier()
-notifier.add_services([Discord, Telegram])
 
 @app.entrypoint
 def main(args):
-
     # 'args' contains your fenn.yaml configurations
-    message = f"Training with learning rate: {args['training']['lr']}"
-
-    notifier.notify(message)
-    print(message)
+    print(f"Training with learning rate: {args['train']['lr']}")
 
     # Your logic here...
 
