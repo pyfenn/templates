1c21a59d32309c381b95e316bd3cf955f9df6cfd
devendra jadhav <devendrajadhav2470@gmail.com>
2026-01-14 21:01:48 +0530

    add CNN template for image classification

---
+++ cnn/modules/model.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN(nn.Module):
+    """
+    A simple CNN for image classification.
+    Default architecture is designed for 32x32 RGB images (e.g., CIFAR-10).
+    """
+
+    def __init__(self, in_channels=3, num_classes=10):
+        super().__init__()
+
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.25)
+
+        # After 3 pooling layers: 32x32 -> 16x16 -> 8x8 -> 4x4
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        # Block 1: Conv -> BN -> ReLU -> Pool
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+
+        # Block 2: Conv -> BN -> ReLU -> Pool
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+
+        # Block 3: Conv -> BN -> ReLU -> Pool
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+
+        # Flatten
+        x = x.view(x.size(0), -1)
+
+        # Fully connected layers
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.fc2(x)
+
+        return x
+
